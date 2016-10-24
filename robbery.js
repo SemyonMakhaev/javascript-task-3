cc5a2e63e97a11ab2785fafdf700f83136674437
Semen Makhaev <semenmakhaev@yandex.ru>
2016-10-25 02:46:06 +0500

    Исправлен tryLater снова

--- robbery.js
+++ robbery.js
@@ -86,7 +86,7 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          */
         tryLater: function () {
             if (typeof this.robberyTime === 'undefined') {
-                return false;
+                this.exists();
             }
             var border = new Date(Number(this.robberyTime) +
                         Number(new Date(1000 * 60 * 30)));
